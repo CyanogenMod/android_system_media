301237a52f7582bb1acde9685ed586ff3cd5c2c1
Jean-Michel Trivi <jmtrivi@google.com>
2010-05-25 17:02:32 -0700

    Add a command line test for PCM buffer queues. This example is based on the buffer queue sample code from the OpenSL ES specification.
    Change-Id: Ic0730549fe54e7d5cedf1bd7d374e2b81c1e969d

---
+++ opensles/tests/pcmBufferQueue/sawtoothBufferQueue/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES:= \
+	$(JNI_H_INCLUDE) \
+	$(TOP)/system/media/opensles/include
+
+LOCAL_SRC_FILES:= \
+    bufferQueue.c \
+	playSawtoothBufferQueue.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+	libutils \
+	libopensles
+
+ifeq ($(TARGET_OS),linux)
+	LOCAL_CFLAGS += -DXP_UNIX
+	#LOCAL_SHARED_LIBRARIES += librt
+endif
+
+LOCAL_MODULE:= slesTest_sawtoothBufferQueue
+
+include $(BUILD_EXECUTABLE)
