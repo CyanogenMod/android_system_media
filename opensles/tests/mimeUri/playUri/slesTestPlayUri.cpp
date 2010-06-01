9ecb3dfd2397fa1970409f4af1eb5ca1c685ef3b
Jean-Michel Trivi <jmtrivi@google.com>
2010-06-01 12:24:25 -0700

    Add command line test to play a URI, to exercise the OpenSL ES AudioPlayer with a SL_DATALOCATOR_URI as data source. Plays 10s of the URI.
    Change-Id: I24b8ae5293c4efbc372551febd91eb6dafe890eb

---
+++ opensles/tests/mimeUri/playUri/slesTestPlayUri.cpp
@@ -0,0 +1,214 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright (c) 2009 The Khronos Group Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and /or associated documentation files (the "Materials "), to deal in the
+ * Materials without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Materials,
+ * and to permit persons to whom the Materials are furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Materials.
+ *
+ * THE MATERIALS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE MATERIALS OR THE USE OR OTHER DEALINGS IN THE
+ * MATERIALS.
+ */
+
+#define LOG_NDEBUG 0
+#define LOG_TAG "slesTestPlayUri"
+
+#include <utils/Log.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#include "OpenSLES.h"
+
+
+#define MAX_NUMBER_INTERFACES 3
+#define MAX_NUMBER_OUTPUT_DEVICES 6
+
+
+//-----------------------------------------------------------------
+/* Checks for error. If any errors exit the application! */
+void CheckErr( SLresult res )
+{
+    if ( res != SL_RESULT_SUCCESS )
+        {
+            fprintf(stdout, "%d SL failure, exiting\n", res);
+            exit(1);
+        }
+    else {
+        //fprintf(stdout, "%d SL success, proceeding...\n", res);
+    }
+}
+
+
+//-----------------------------------------------------------------
+
+/* Play some music from a buffer queue  */
+void TestPlayUri( SLObjectItf sl, const char* path)
+{
+    SLEngineItf                EngineItf;
+
+    SLint32                    numOutputs = 0;
+    SLuint32                   deviceID = 0;
+
+    SLresult                   res;
+
+    SLDataSource               audioSource;
+    SLDataLocator_URI          uri;
+    SLDataFormat_MIME          mime;
+
+    SLDataSink                 audioSink;
+    SLDataLocator_OutputMix    locator_outputmix;
+
+    SLObjectItf                player;
+    SLPlayItf                  playItf;
+    SLVolumeItf                volItf;
+    SLBufferQueueItf           bufferQueueItf;
+    SLBufferQueueState         state;
+
+    SLObjectItf                OutputMix;
+
+    int                        i;
+
+    SLboolean required[MAX_NUMBER_INTERFACES];
+    SLInterfaceID iidArray[MAX_NUMBER_INTERFACES];
+
+    /* Get the SL Engine Interface which is implicit */
+    res = (*sl)->GetInterface(sl, SL_IID_ENGINE, (void*)&EngineItf);
+    CheckErr(res);
+
+    /* Initialize arrays required[] and iidArray[] */
+    for (i=0;i<MAX_NUMBER_INTERFACES;i++)
+        {
+            required[i] = SL_BOOLEAN_FALSE;
+            iidArray[i] = SL_IID_NULL;
+        }
+
+    // Set arrays required[] and iidArray[] for VOLUME interface
+    required[0] = SL_BOOLEAN_TRUE;
+    iidArray[0] = SL_IID_VOLUME;
+    // Create Output Mix object to be used by player
+    res = (*EngineItf)->CreateOutputMix(EngineItf, &OutputMix, 1,
+            iidArray, required); CheckErr(res);
+
+    // Realizing the Output Mix object in synchronous mode.
+    res = (*OutputMix)->Realize(OutputMix, SL_BOOLEAN_FALSE);
+    CheckErr(res);
+
+    /* Setup the data source structure for the URI */
+    uri.locatorType = SL_DATALOCATOR_URI;
+    uri.URI         =  (SLchar*) path;
+    mime.formatType    = SL_DATAFORMAT_MIME;
+    /*mime.mimeType      = (SLchar*)"audio/mpeg";
+    mime.containerType = SL_CONTAINERTYPE_MP3;*/
+
+    audioSource.pFormat      = (void *)&mime;
+    audioSource.pLocator     = (void *)&uri;
+
+    /* Setup the data sink structure */
+    locator_outputmix.locatorType   = SL_DATALOCATOR_OUTPUTMIX;
+    locator_outputmix.outputMix    = OutputMix;
+    audioSink.pLocator           = (void *)&locator_outputmix;
+    audioSink.pFormat            = NULL;
+
+    /* Create the audio player */
+    res = (*EngineItf)->CreateAudioPlayer(EngineItf, &player,
+            &audioSource, &audioSink, 1, iidArray, required); CheckErr(res);
+
+    /* Realizing the player in synchronous mode. */
+    res = (*player)->Realize(player, SL_BOOLEAN_FALSE); CheckErr(res);
+    fprintf(stdout, "URI example: after Realize\n");
+
+    /* Get play and vol interfaces */
+    res = (*player)->GetInterface(player, SL_IID_PLAY, (void*)&playItf);
+    CheckErr(res);
+
+    res = (*player)->GetInterface(player, SL_IID_VOLUME,
+                (void*)&volItf); CheckErr(res);
+    CheckErr(res);
+
+    /* Set the player volume */
+    res = (*volItf)->SetVolumeLevel( volItf, -300);
+    CheckErr(res);
+
+    /* Play the URI */
+    fprintf(stdout, "URI example: starting to play\n");
+    res = (*playItf)->SetPlayState( playItf, SL_PLAYSTATE_PLAYING );
+    CheckErr(res);
+
+    /* Play 10s of the URI */
+    usleep(10 * 1000 * 1000);
+
+    /* Make sure player is stopped */
+    fprintf(stdout, "URI example: stopping playback\n");
+    res = (*playItf)->SetPlayState(playItf, SL_PLAYSTATE_STOPPED);
+    CheckErr(res);
+
+    /* Destroy the player */
+    (*player)->Destroy(player);
+
+    /* Destroy Output Mix object */
+    (*OutputMix)->Destroy(OutputMix);
+}
+
+//-----------------------------------------------------------------
+int main(int argc, char* const argv[])
+{
+    LOGV("Starting slesTestPlayUri\n");
+
+    SLresult    res;
+    SLObjectItf sl;
+
+    if (argc == 1) {
+        fprintf(stdout, "Usage: \n\t%s path \n\t%s url\n", argv[0], argv[0]);
+        fprintf(stdout, "Example: \"%s /sdcard/my.mp3\"  or \"%s file:///sdcard/my.mp3\"\n", argv[0], argv[0]);
+        exit(1);
+    }
+
+    SLEngineOption EngineOption[] = {
+            {(SLuint32) SL_ENGINEOPTION_THREADSAFE,
+            (SLuint32) SL_BOOLEAN_TRUE}};
+
+    res = slCreateEngine( &sl, 1, EngineOption, 0, NULL, NULL);
+    CheckErr(res);
+    /* Realizing the SL Engine in synchronous mode. */
+    res = (*sl)->Realize(sl, SL_BOOLEAN_FALSE);
+    CheckErr(res);
+
+    TestPlayUri(sl, argv[1]);
+
+    /* Shutdown OpenSL ES */
+    (*sl)->Destroy(sl);
+    exit(0);
+
+    return 0;
+}
