d531cc5dec42825ed8003fd98394039c235ac4b2
Jean-Michel Trivi <jmtrivi@google.com>
2010-06-09 09:47:00 -0700

    New OpenSL ES test+demo application that uses the SLPrefetchStatusItf. Plays two URIs and pans them left and right.
    Change-Id: Ic804c0d666889088db6a4075f5c40029e11273fe

---
+++ opensles/tests/mimeUri/playUri2/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := tests
+
+LOCAL_C_INCLUDES:= \
+	$(JNI_H_INCLUDE) \
+	$(TOP)/system/media/opensles/include
+
+LOCAL_SRC_FILES:= \
+	slesTestPlayUri.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+	libutils \
+	libopensles
+	
+
+ifeq ($(TARGET_OS),linux)
+	LOCAL_CFLAGS += -DXP_UNIX
+endif
+
+LOCAL_MODULE:= slesTest_playUri2
+
+include $(BUILD_EXECUTABLE)
