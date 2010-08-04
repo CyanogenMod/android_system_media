68c8d376063a53b47c18dc99331253fb70bd695b
Glenn Kasten <gkasten@google.com>
2010-08-03 17:51:09 -0700

    Add the seek torture test
    Change-Id: Icd4ef20283f6f0ac499901a7ffa8e9416abb2798

--- opensles/tests/mimeUri/Android.mk
+++ opensles/tests/mimeUri/Android.mk
@@ -146,3 +146,28 @@ endif
 LOCAL_MODULE:= slesTest_playUri2
 
 include $(BUILD_EXECUTABLE)
+
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := tests
+
+LOCAL_C_INCLUDES:= \
+	$(JNI_H_INCLUDE) \
+	system/media/opensles/include
+
+LOCAL_SRC_FILES:= \
+	seekTorture.c
+
+LOCAL_SHARED_LIBRARIES := \
+	libutils \
+	libOpenSLES
+
+ifeq ($(TARGET_OS),linux)
+	LOCAL_CFLAGS += -DXP_UNIX
+endif
+
+LOCAL_MODULE:= slesTest_seekTorture
+
+include $(BUILD_EXECUTABLE)
