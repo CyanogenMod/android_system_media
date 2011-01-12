439af19ad7484b0472080e8eb0cd8462a17b0286
Glenn Kasten <gkasten@google.com>
2011-01-12 07:29:49 -0800

    Directory re-organization
    Change-Id: I8e3f12a22e765d36ddefd87edf204735b25474f6

---
+++ wilhelm/tests/native-media/jni/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := tests
+LOCAL_MODULE    := libnative-media-jni
+LOCAL_SRC_FILES := native-media-jni.c
+LOCAL_CFLAGS += -Isystem/media/wilhelm/include
+
+LOCAL_PRELINK_MODULE := false
+LOCAL_SHARED_LIBRARIES += libutils libOpenSLES libandroid
+
+include $(BUILD_SHARED_LIBRARY)
