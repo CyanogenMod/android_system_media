ae137c1786f3f3b30c3e4000da8c033477b1b343
Ruben Brunk <rubenbrunk@google.com>
2014-07-01 23:55:06 +0000

    Revert "camera2: Add legacy mode enum to supported hardware level."
    This reverts commit 101aa5ec543f0aecd147399dc085fc9355d0ddae.
    
    Change-Id: I03585a885312558a136dfced4d6f54c40fa0dab8

--- camera/src/camera_metadata_tag_info.c
+++ camera/src/camera_metadata_tag_info.c
@@ -2234,10 +2234,6 @@ int camera_metadata_enum_snprint(uint32_t tag,
                     msg = "FULL";
                     ret = 0;
                     break;
-                case ANDROID_INFO_SUPPORTED_HARDWARE_LEVEL_LEGACY:
-                    msg = "LEGACY";
-                    ret = 0;
-                    break;
                 default:
                     msg = "error: enum value out of range";
             }
