be30d4fcf4e983ec42eecf73a53317083a23a89a
Zhijun He <zhijunhe@google.com>
2015-01-12 15:06:16 -0800

    camera_metadata: add lens facing EXTERNAL enum
    This is for usb camera support
    
    Change-Id: Id8329e31ad0622bdc06c60e87e97c742a9719b3b

--- camera/src/camera_metadata_tag_info.c
+++ camera/src/camera_metadata_tag_info.c
@@ -1441,6 +1441,10 @@ int camera_metadata_enum_snprint(uint32_t tag,
                     msg = "BACK";
                     ret = 0;
                     break;
+                case ANDROID_LENS_FACING_EXTERNAL:
+                    msg = "EXTERNAL";
+                    ret = 0;
+                    break;
                 default:
                     msg = "error: enum value out of range";
             }
