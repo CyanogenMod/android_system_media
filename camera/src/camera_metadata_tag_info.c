796e574d0d0bff63c00fd9dacd68912f3da50e14
Eino-Ville Talvala <etalvala@google.com>
2015-04-14 13:13:00 -0700

    Camera: Add HIGH_RESOLUTION hardware level.
    For camera devices that support very high resolutions, add
    a level that allows for those resolutions to operate at lower
    capture rates, while ensuring that a reasonable resolution is
    still available for high-rate capture.
    
    Change-Id: I06be831ddbd59db58b76305c6125d2bff18aa1b8

--- camera/src/camera_metadata_tag_info.c
+++ camera/src/camera_metadata_tag_info.c
@@ -2427,6 +2427,10 @@ int camera_metadata_enum_snprint(uint32_t tag,
                     msg = "LEGACY";
                     ret = 0;
                     break;
+                case ANDROID_INFO_SUPPORTED_HARDWARE_LEVEL_HIGH_RESOLUTION:
+                    msg = "HIGH_RESOLUTION";
+                    ret = 0;
+                    break;
                 default:
                     msg = "error: enum value out of range";
             }
