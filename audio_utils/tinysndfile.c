2c29cee2e519db2decc91747b01f54597a856d1b
Glenn Kasten <gkasten@google.com>
2015-06-08 16:12:07 -0700

    Add FCC_8 comments to indicate 8-channel assumptions
    Bug: 21656069
    Change-Id: I411a5c83eec126bc6cda796bd4c9f842ed6ee762

--- audio_utils/tinysndfile.c
+++ audio_utils/tinysndfile.c
@@ -181,6 +181,7 @@ static SNDFILE *sf_open_read(const char *path, SF_INFO *info)
                 fseek(stream, (long) (chunkSize - minSize), SEEK_CUR);
             }
             unsigned channels = little2u(&fmt[2]);
+            // FIXME FCC_8
             if (channels != 1 && channels != 2 && channels != 4 && channels != 6 && channels != 8) {
 #ifdef HAVE_STDERR
                 fprintf(stderr, "unsupported channels %u\n", channels);
@@ -298,6 +299,7 @@ static SNDFILE *sf_open_write(const char *path, SF_INFO *info)
     int sub = info->format & SF_FORMAT_SUBMASK;
     if (!(
             (info->samplerate > 0) &&
+            // FIXME FCC_8
             (info->channels > 0 && info->channels <= 8) &&
             ((info->format & SF_FORMAT_TYPEMASK) == SF_FORMAT_WAV) &&
             (sub == SF_FORMAT_PCM_16 || sub == SF_FORMAT_PCM_U8 || sub == SF_FORMAT_FLOAT ||
