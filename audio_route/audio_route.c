2b384d548134c76665c4f77fade862ce38de218f
Rashed Abdel-Tawab <rashed@linux.com>
2016-08-25 15:49:35 -0400

    Revert "Nullify non-initialized entries in the mixer control structure"
    This reverts commit 5274ad25faa1a16ee79d95f8c56917a753f09e87.
    
    Change-Id: I54346efd9f872b7e4883701f24fb03dfcf5710b3

--- audio_route/audio_route.c
+++ audio_route/audio_route.c
@@ -578,12 +578,8 @@ static int alloc_mixer_state(struct audio_route *ar)
         /* Skip unsupported types that are not supported yet in XML */
         type = mixer_ctl_get_type(ctl);
 
-        if (!is_supported_ctl_type(type)) {
-            ar->mixer_state[i].old_value = NULL;
-            ar->mixer_state[i].new_value = NULL;
-            ar->mixer_state[i].reset_value = NULL;
+        if (!is_supported_ctl_type(type))
             continue;
-        }
 
         size_t value_sz = sizeof_ctl_type(type);
         ar->mixer_state[i].old_value.ptr = calloc(num_values, value_sz);
